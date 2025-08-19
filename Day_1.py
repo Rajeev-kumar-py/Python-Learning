360b6b2dc761e91059a8fd80f7f8e10181e8b046
Rajeev-kumar-py <rajeevkumar18245@gmail.com>
2025-08-19 23:24:49 +0530

    Added day_1.py

---
+++ Day_1.py
@@ -0,0 +1,20 @@
+# Day 1 - Python Basics
+
+# 1. Variables
+name = "Raj"
+age = 20
+pi = 3.14
+is_coding = True
+
+print("Name:", name)
+print("Age:", age)
+print("Pi Value:", pi)
+print("Is Coding?", is_coding)
+
+# 2. Input/Output
+user_name = input("Enter your name: ")
+print("Hello,", user_name)
+
+# 3. Type Conversion
+num = int(input("Enter a number: "))
+print("Number + 5 =", num + 5)
